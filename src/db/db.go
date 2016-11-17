8cf4c97afa2b5f95178118f0d2d3128fd9a39e67
sirrah23 <hseth93@gmail.com>
2016-11-17 00:41:13 -0500

    Added a get key function for database

--- src/db/db.go
+++ src/db/db.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 func add_entry(file_name, key, value string) {
@@ -32,6 +35,7 @@ func del_entry(file_name, key string) {
 	checkError(err)
 	//TODO : Create a Close_Database(...) function for this
 	defer fhandle.Close()
+	//TODO : Check if key is in database before delete
 	entry := key + "-DELETED\n" // Special tombstone marker
 	_, err = fhandle.WriteString(entry)
 	checkError(err)
@@ -40,6 +44,35 @@ func del_entry(file_name, key string) {
 func checkError(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func Split_In_Two(str_to_split, sep string) (string, string) {
+	s := strings.Split(str_to_split, sep)
+	return s[0], s[1]
+
+}
+
+func get(file_name, key string) string {
+	fhandle, err := os.Open(file_name)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fhandle, err = os.Create(file_name)
+		}
+	}
+	checkError(err)
+	defer fhandle.Close()
+	file_reader := bufio.NewReader(fhandle)
+	for {
+		data, err := file_reader.ReadString('\n')
+		if err == io.EOF {
+			// TODO: Return error here?
+			return ""
+		}
+		curr_key, curr_val := Split_In_Two(data, ":")
+		if curr_key == key {
+			return curr_val[:len(curr_val)-1]
+		}
 	}
 }
 
@@ -60,8 +93,10 @@ func main() {
 		add_entry(file, key, value)
 	} else if action == "del" {
 		del_entry(file, key)
+	} else if action == "get" {
+		fmt.Println(get(file, key))
 	} else {
-		fmt.Println("Usage: db (add|del) key [value]")
+		fmt.Println("Usage: db (add|del|get) key [value]")
 		return
 	}
 }
